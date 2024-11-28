2b08590d38744f74e26ed9a7fb4a8b5f163d2940
hali-27 <halima_k@hotmail.co.uk>
2024-11-28 22:01:38 +0000

    add user input

---
+++ index.js
@@ -0,0 +1,9 @@
+const quizData = [
+    {
+        question: "What is the capital of France?",
+        answers: [
+            { text: "New York", correct: false },
+            { text: "Paris", correct: true },
+            { text: "London", correct: false },
+            { text: "Madrid", correct: false },
+    }];
